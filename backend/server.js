487037bb18fe637484068bce1efb061bd24bc3b5
prathameshpawar <141353883+prathameshpawar12@users.noreply.github.com>
2025-03-10 21:44:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const db = mysql.createConnection({
   password:process.env.DB_PASSWORD, 
   database:process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
-  ssl: { rejectUnauthorized: true }
+  // ssl: { rejectUnauthorized: true }
 });
 
 db.connect((err) => {
